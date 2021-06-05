e91089284acd35542d60aea08094fcae06236e8d
전호영 <78394999+HoyeongJeon@users.noreply.github.com>
2021-06-05 15:33:13 +0900

    c express 챕터 10 01번

--- C_Express/main.c
+++ C_Express/main.c
@@ -1,14 +1,14 @@
-//
-//  main.c
-//  C_Express
-//
-//  Created by Hoyoung Jeon on 2021/06/05.
-//
+// Chapter 10_01
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void)
+{
+    int days[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    printf("%d월은 %d일까지 있습니다.\n", 8, days[7]);
+    printf("%d월은 %d일까지 있습니다.\n", 9, days[8]);
+    printf("%d월은 %d일까지 있습니다.\n", 10, days[9]);
+    printf("%d월은 %d일까지 있습니다.\n", 11, days[10]);
+    printf("%d월은 %d일까지 있습니다.\n", 12, days[11]);
     return 0;
 }
